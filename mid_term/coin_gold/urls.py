6f0ff8cdf55052bb8aea7fee00b975424140afc2
nghia <trongnghiabui0807@gmail.com>
2024-10-31 18:47:11 +0700

    .

--- mid_term/coin_gold/urls.py
+++ mid_term/coin_gold/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('', views.coin_list, name='coin_list'),
+    path('gold/', views.gold_list, name='gold_list')
 ]
--- mid_term/coin_gold/views.py
+++ mid_term/coin_gold/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
 
 def coin_list(request):
-    return render(request, '')
+    coin_url = 'https://apiforlearning.zendvn.com/api/get-coin'
+    coin_respond = requests.get(coin_url, verify=False)
+    items_coin = coin_respond.json()
+
+    return render(request, 'coin.html', {"items_coin": items_coin})
+
+def gold_list(request):
+    gold_url = 'https://apiforlearning.zendvn.com/api/get-gold'
+    gold_respond = requests.get(gold_url, verify=False)
+    items_gold = gold_respond.json()
+
+    return render(request, 'gold.html', {"items_gold": items_gold})
